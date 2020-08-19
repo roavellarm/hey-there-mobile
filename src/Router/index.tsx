2d7217fdba501914edead10d31c4fd3fdf243fd1
Rodrigo Avellar <roavellarm@gmail.com>
2020-08-18 22:00:25 -0300

    Update Router

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import Header from '../components/Header'
 import ChatList from '../pages/ChatList'
+import Profile from '../pages/Profile'
 
 const { Navigator, Screen } = createStackNavigator()
 
 const Router: React.FC = () => {
   return (
     <NavigationContainer>
-      <Navigator>
-        <Screen name="Chat List" component={ChatList} />
+      <Navigator
+        screenOptions={{
+          headerTitle: () => <Header />,
+          headerStyle: {
+            backgroundColor: '#574b5d',
+            height: 80,
+          },
+        }}
+      >
+        <Screen name="Chats" component={ChatList} />
+        <Screen name="Profile" component={Profile} />
       </Navigator>
     </NavigationContainer>
   )
