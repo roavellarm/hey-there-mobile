e25916422e786ebb7ea3c027a7ddb0f36f193f9f
Rodrigo Avellar <roavellarm@gmail.com>
2020-08-20 19:40:32 -0300

    Add Title Component

---
+++ src/components/Title/index.tsx
@@ -0,0 +1,20 @@
+import React, { ReactNode } from 'react'
+import styled from 'styled-components/native'
+
+const StyledTitle = styled.Text`
+  font-family: 'Lato_300Light';
+  line-height: 100px;
+  letter-spacing: 0.4px;
+  font-size: 32px;
+  color: #fff;
+`
+
+interface Props {
+  children: ReactNode
+}
+
+const Title: React.FC<Props> = ({ children }: Props) => {
+  return <StyledTitle>{children}</StyledTitle>
+}
+
+export default Title
