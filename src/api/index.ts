ff2430bcb3bd06ec46e26af1cf2a224aedbb7829
Gustavo <gustavosohne38@gmail.com>
2020-09-17 16:50:07 -0300

    end

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.15:3001',
-  // 'https://heythere-staging-api.herokuapp.com',
+  baseURL: 'https://heythere-staging-api.herokuapp.com',
+
+  // 'http://192.168.0.15:3001',
 })
 
 export default api
