dedee0bc4befe1c22ccc3e8bdf7c39da2b1df90e
Rodrigo Avellar <roavellarm@gmail.com>
2020-09-17 16:19:25 -0300

    Add Axios interceptor

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,21 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.15:3001',
-  // 'https://heythere-staging-api.herokuapp.com',
+  baseURL: 'http://192.168.1.12:3001',
 })
+
+api.interceptors.response.use(
+  config => config,
+  error => {
+    // if (error.response === undefined) {
+    //   const networkError = { error: JSON.parse(JSON.stringify(error)).message }
+    //   return Promise.reject(networkError)
+    // }
+
+    const { status, data } = error.response
+    if (status === 400) return Promise.reject(data)
+    return Promise.reject(error)
+  }
+)
 
 export default api
