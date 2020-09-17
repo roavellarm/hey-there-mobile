fd3bb810f5676ea8a41b7473345e15a27c46c1aa
Gustavo <gustavosohne38@gmail.com>
2020-09-17 17:26:51 -0300

    config

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,14 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-<<<<<<< HEAD
-  baseURL: 'https://heythere-staging-api.herokuapp.com',
-
-  // 'http://192.168.0.15:3001',
-=======
   // baseURL: 'http://192.168.1.12:3001',
   baseURL: 'https://heythere-staging-api.herokuapp.com',
->>>>>>> bc6373c2cd54b2dee5aaeee3260538689eae2c83
 })
 
 api.interceptors.response.use(
